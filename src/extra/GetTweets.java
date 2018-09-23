521af8e26dc84cabca36a9a35d1af4f16e17c82a
league <league@wts9.attlocal.net>
2018-09-22 17:25:56 -0700

    Finished GetTweets PigLatinTranslator (IP)

--- src/extra/GetTweets.java
+++ src/extra/GetTweets.java
@@ -5,14 +5,22 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
 
 public class GetTweets implements ActionListener {
 	JFrame J = new JFrame();
 	JPanel P = new JPanel();
 	JTextField T = new JTextField(40);
 	JButton b1 = new JButton();
+	JLabel JL = new JLabel();
 public static void main(String[] args) {
 	GetTweets GT = new GetTweets();
 	GT.showButton();
@@ -20,6 +28,7 @@ public static void main(String[] args) {
 public void showButton() {
 	J.add(P);
 	P.add(b1);
+	P.add(JL);
 	P.add(T);
 	b1.setText("Search Twitter");
 	J.setVisible(true);
@@ -29,8 +38,32 @@ public void showButton() {
 @Override
 public void actionPerformed(ActionEvent e) {
 	
-	if(e.equals(b1)) {
-		System.out.println("Tweet, Tweet");
+	if(e.getSource()== b1) {
+		JL.setText(getLatestTweet(T.getText()));
+		J.pack();
 	}
 }
+private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken(
+          "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+          "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+          "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+
+    twitter.setOAuthAccessToken(accessToken);
+
+    Query query = new Query(searchingFor);
+    try {
+          QueryResult result = twitter.search(query);
+          return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+          System.err.print(e.getMessage());
+          return "What the heck is that?";
+    }
+}
+
 }
