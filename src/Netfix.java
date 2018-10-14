3dd86fee8a6e9c19e51d28d8e10e8e9b87c71695
league <league@wts22.attlocal.net>
2018-10-13 17:25:55 -0700

    Netflix IP

--- src/Netfix.java
+++ src/Netfix.java
@@ -1,6 +1,21 @@
 
 public class Netfix {
 	public static void main(String[] args) {
-
+		Movie m = new Movie("Twilight", 0);
+		Movie m2 = new Movie("Infinity War", 5);
+		Movie m3 = new Movie("Venom", 3);
+		Movie m4 = new Movie("The Isle of Dogs", 4);
+		Movie m5 = new Movie("The Shining", 5);
+		m.getTitle();
+		m.getRating();
+		m.getTicketPrice();
+		NetflixQueue netflix = new NetflixQueue();
+		netflix.addMovie(m);
+		netflix.addMovie(m2);
+		netflix.addMovie(m3);
+		netflix.addMovie(m4);
+		netflix.addMovie(m5);
+		netflix.getBestMovie();
+		System.out.println(netflix);
 	}
 }
--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 import java.util.Random;
 
@@ -8,7 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements ActionListener {
 	JFrame J = new JFrame();
 	JPanel P = new JPanel();
 	JButton b1 = new JButton();
@@ -17,6 +19,7 @@ public class SlotMachine {
 	JLabel JL3 = new JLabel();
 	Random r = new Random();
 	int i = r.nextInt(3);
+	String score;
 
 	public static void main(String[] args) {
 		SlotMachine s = new SlotMachine();
@@ -24,23 +27,44 @@ public class SlotMachine {
 	}
 
 	public void showButton() {
-		String seven = new String();
-		seven = "7.jpg";
+
 		J.add(P);
+		J.setVisible(true);
 		J.setTitle("Spin to Win");
 		P.add(b1);
 		P.add(JL);
 		P.add(JL2);
 		P.add(JL3);
+		P.setVisible(true);
 		b1.setText("Spin!");
-		if (i == 1) {
-			JL = loadImageFromComputer(seven);
-		}
+		b1.addActionListener(this);
+
+		J.pack();
 	}
 
 	public JLabel loadImageFromComputer(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String seven = new String();
+		seven = "7.jpg";
+		String cherry = new String();
+		cherry = "Cherries-512.png";
+		String bar = new String();
+		bar = "bar.png";
+		if (i == 1) {
+			JL = loadImageFromComputer(seven);
+			J.pack();
+		} else if (i == 2) {
+			JL = loadImageFromComputer(cherry);
+			J.pack();
+		} else if (i == 3) {
+			JL = loadImageFromComputer(bar);
+			J.pack();
+		}
 	}
 }
